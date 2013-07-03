0c2ac79798ab82391ba131664d19174a4a24c3b9
dkhanevich <dkhanevich@d8ebeebf-d64f-4e48-9a47-8608552ece91>
2013-07-03 13:17:31 +0000

    Fix IS-218
    git-svn-id: https://svn.omertex.com:8444/svn/iSimple@353 d8ebeebf-d64f-4e48-9a47-8608552ece91

--- sources/isimple-project/src/com/treelev/isimple/app/ISimpleApp.java
+++ sources/isimple-project/src/com/treelev/isimple/app/ISimpleApp.java
@@ -1,16 +1,33 @@
 package com.treelev.isimple.app;
 
 import android.location.Location;
+import com.treelev.isimple.R;
 import com.treelev.isimple.data.ShopDAO;
 import com.treelev.isimple.domain.ui.AbsDistanceShop;
 import org.holoeverywhere.app.Application;
 
 import java.util.List;
 
+import org.acra.*;
+import org.acra.annotation.*;
+
+@ReportsCrashes(
+    formKey = "",
+    mailTo = "dkhanevich@gmail.com, dv@treelev.com, dshaplyko@omertex.com",
+    mode = ReportingInteractionMode.DIALOG,
+    resDialogTitle = R.string.crash_dialog_title,
+    resDialogText = R.string.crash_dialog_text
+)
 public class ISimpleApp extends Application {
 
     private List<AbsDistanceShop> distanceShopList;
     private Location currentLocation;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ACRA.init(this);
+    }
 
     public void reloadShopList() {
         distanceShopList = new ShopDAO(this).getNearestShops(currentLocation);
